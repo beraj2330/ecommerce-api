f5de57fdfc6ed4b779f035b22f9ef55f0f0e0122
Jeel Bera <jeelbera2330@gmail.com>
2024-11-07 15:46:15 +0530

    Created user service for creating a new user in database and displaying all user from database.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) {}
+
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const user = this.userRepository.create(createUserDto);
+        return this.userRepository.save(user);
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+}
+
