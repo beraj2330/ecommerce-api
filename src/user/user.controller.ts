8ebcdcd75d9cb0382e2d16d26ee8ab0a7c967c75
Jeel Bera <jeelbera2330@gmail.com>
2024-11-07 16:16:43 +0530

    Initialized

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private userService: UserService) {}
+
+    @Post()
+    createUser(@Body()  createUserDto: CreateUserDto) {
+        return this.userService.createUser(createUserDto);
+    }
+
+    @Get()
+    getAll() {
+        return this.userService.findAll();
+    }
+}
