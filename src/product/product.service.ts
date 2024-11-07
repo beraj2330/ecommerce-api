798e8a55cddfc36a844c8cbac737ea830ea5d111
Jeel Bera <jeelbera2330@gmail.com>
2024-11-07 16:54:39 +0530

    Created bussiness logic and databse logic to create and get all data for products

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+
+    constructor(
+        @InjectRepository(Product)
+        private productRepository: Repository<Product>) {}
+
+    async createProduct(createProductDto: CreateProductDto): Promise<Product> {
+
+        const product = this.productRepository.create(createProductDto);
+        return this.productRepository.save(product);
+    }
+
+    async findAll(): Promise<Product[]> {
+        return this.productRepository.find();
+    }
+}
