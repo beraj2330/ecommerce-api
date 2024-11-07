846cf6ab9b37faee2a029ba12edd2e0da504f7d0
Jeel Bera <jeelbera2330@gmail.com>
2024-11-07 17:00:04 +0530

    Product controller for incoming requests

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    constructor(private productService: ProductService) {}
+
+    @Post()
+    createProduct(@Body() createProductDto: CreateProductDto) {
+        return this.productService.createProduct(createProductDto);
+    }
+
+    @Get()
+    getAll() {
+        return this.productService.findAll();
+    }
+}
