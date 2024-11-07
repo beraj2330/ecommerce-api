e816b8d4786b0d8d0b4884d5020091548f96b122
Jeel Bera <jeelbera2330@gmail.com>
2024-11-07 17:59:32 +0530

    Added swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('API for an e-commerce platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
