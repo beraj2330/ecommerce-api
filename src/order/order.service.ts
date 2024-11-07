6dd1cafabc5e488a43967901b24790aaa741e8d3
Jeel Bera <jeelbera2330@gmail.com>
2024-11-07 18:56:34 +0530

    Created Repository

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './order.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+
+    constructor(
+        @InjectRepository(Order)
+        private orderRepository: Repository<Order> ) {}
+
+    
+}
