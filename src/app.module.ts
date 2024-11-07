b85c084f8705d59d27b9851552421f16b20d366f
Jeel Bera <jeelbera2330@gmail.com>
2024-11-07 18:13:20 +0530

    Initialized all the files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 
 @Module({
@@ -18,6 +19,7 @@ import { ProductModule } from './product/product.module';
     }),
     UserModule,
     ProductModule,
+    OrderModule,
     
   ],
 })
