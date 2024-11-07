8e6eb36ac8442bd799b1f8f3e284e9c277ce855a
Jeel Bera <jeelbera2330@gmail.com>
2024-11-07 13:57:35 +0530

    For creation

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, IsEmail, MinLength } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(6)
+    password: string;
+
+    @IsString()
+    role: 'customer' | 'vendor' | 'admin';
+}
