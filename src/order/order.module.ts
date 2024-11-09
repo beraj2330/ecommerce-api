e7aac8ec4cf744e7ff9ee104f26083761d57aa93
Jeel Bera <jeelbera2330@gmail.com>
2024-11-09 21:21:07 +0530

    updated the Order module to import order, user and product entity

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './order.entity';
+import { User } from 'src/user/user.entity';
+import { Product } from 'src/product/product.entity';
 
 @Module({
   providers: [OrderService],
-  controllers: [OrderController]
+  controllers: [OrderController],
+  imports:[TypeOrmModule.forFeature([Order, User, Product])],
 })
 export class OrderModule {}
