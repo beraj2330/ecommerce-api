a1957dce420e22b4f7a12e3dca129c7f983073e7
Jeel Bera <jeelbera2330@gmail.com>
2024-11-10 10:49:12 +0530

    added the hash method

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -16,8 +16,12 @@ export class User {
     @Column({ default: 'customer'})
     role: string;
 
+    //this decorator is used to TypeORM to specify that a 
+    //method should run automatically before a new record is saved in the database
     @BeforeInsert()
+    //Inside this method password hashing logic is implemented
     async hashPassword() {
+        
         this.password = await bcrypt.hash(this.password, 10);
     }
 }
